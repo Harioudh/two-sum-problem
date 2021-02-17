8cd6b9fc8518f6ab4788ba278c2055a06e8bc834
Akash Harioudh <akashharioudh@gmail.com>
2021-02-17 06:30:36 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std ;
+
+vector<int> find2Sum(vector<int> nums, int numSize,int target)
+{
+  // better solution 
+  //1.sor the array
+
+  sort(nums.begin(),nums.end());
+
+  //take two pointers
+
+  int left = 0;
+  int right = nums.size()-1;
+
+  while(left<right)
+  {
+    int currentSum=nums[left]+nums[right];
+    if(currentSum ==target)
+    {
+      return{nums[left],nums[right]};
+
+    }
+
+    else if(currentSum< target)
+    {
+    left++;
+    }
+
+    else if(currentSum>target)
+    {
+      right--;
+    }
+  }
+
+  return {};
+}
+
+int main()
+{
+  vector <int> nums={0,2,7,11,15,3,8,14 };
+  int numSize;
+
+
+  int target=14;
+
+  //find the numbers using for loop.
+
+  vector<int> result=find2Sum(nums,numSize,target);
+
+  for(int i=0;i<2;i++)
+  {
+   printf("%d \t",result[i]);
+
+
+  }
+
+}
+
+  
+
+  
+
+
